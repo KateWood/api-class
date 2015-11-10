ce8f070a9b3bc92cca14e1c02cb9ac12444fa806
Kate Wood <katewood611@gmail.com>
2015-11-10 15:01:09 -0800

    adds routes folder and api js

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 var express	= require('express'),
-	app = express()
+	app		= express(),
+	port	= process.env.PORT || 3000,
+	apiRoutes = require('./routes/api.js')
+
+app.get('/', function(req, res) {
+	res.json({message: "Welcome to my app!"})
+})
+
+app.listen(port, function() {
+	console.log("Server is running on", port)
+})
