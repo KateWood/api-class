69c112041a781c61a667e857e572762682c8ce6b
Kate Wood <katewood611@gmail.com>
2015-11-10 16:29:05 -0800

    get request parses json from response to rest query

--- routes/giphy.js
+++ routes/giphy.js
@@ -2,5 +2,11 @@ var	express = require('express'),
 	giphy	= express.Router(),
 	request = require('request')
 
+giphy.get('/:query', function(req, res) {
+	var searchString = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC"
+	request(searchString, function(error, response, body) {
+		res.send(JSON.parse(body))
+	})
+})
 
 module.exports = giphy
