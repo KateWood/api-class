3a4a47a523ba8663d7d0366fd2d3e7187668d214
Kate Wood <katewood611@gmail.com>
2015-11-10 16:37:27 -0800

    display first giphy based on search params

--- routes/giphy.js
+++ routes/giphy.js
@@ -5,7 +5,8 @@ var	express = require('express'),
 giphy.get('/:query', function(req, res) {
 	var searchString = "http://api.giphy.com/v1/gifs/search?q=" + req.params.query + "&api_key=dc6zaTOxFJmzC"
 	request(searchString, function(error, response, body) {
-		res.send(JSON.parse(body))
+		var imageSource = JSON.parse(body).data[0].images.fixed_height.url
+		res.send('<img src=" ' + imageSource + '">')
 	})
 })
 
